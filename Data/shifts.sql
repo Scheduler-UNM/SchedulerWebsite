3e27d80afe01b1520e24bdd8bdcbea7cc308e77b
Alpharceus <72083371+Alpharceus@users.noreply.github.com>
2023-12-20 23:00:07 +0000

    updated shifts.sql

--- Data/shifts.sql
+++ Data/shifts.sql
@@ -6,6 +6,6 @@ Shift_ID	Day	Time	Pod	No_of_Scons
 1	Monday	16:00-00:00	Pod A	80
 4	Monday	16:00-00:00	Pod B	73
 2	Monday	16:00-00:00	Pod C	36
-5	Monday	16:00-00:00	Pod D	68
+5	Monday	15:00-00:00	Pod D	68
 5	Monday	00:00-08:00	Pod A	76
 3	Monday	00:00-08:00	Pod B	76
