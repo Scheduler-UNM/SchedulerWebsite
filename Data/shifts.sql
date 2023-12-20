5ac29c5bead5ddafdcf417707493dc43d8634e1a
mohansaikumarmadaka <154356154+mohansaikumarmadaka@users.noreply.github.com>
2023-12-20 16:29:22 -0700

    Update shifts.sql

--- Data/shifts.sql
+++ Data/shifts.sql
@@ -1,6 +1,6 @@
 Shift_ID	Day	Time	Pod	No_of_Scons
 7      	Monday	08:00-16:00	Pod A	24
-2	      Monday	08:00-16:00	Pod B	74
+2	     Monday	08:00-16:00	Pod B	74
 2     	Monday	08:00-16:00	Pod C	79
 5      	Monday	08:00-16:00	Pod D	92
 1     	Monday	16:00-00:00	Pod A	80
