42af9430e27e5ed2e78741ec62686f82c84a969e
mohansaikumarmadaka <154356154+mohansaikumarmadaka@users.noreply.github.com>
2023-12-21 11:48:32 -0700

    Update employees.sql

--- Data/employees.sql
+++ Data/employees.sql
@@ -1,11 +1,1 @@
-ID	Name Position	Status	Hours Remaining
-7	Monday	08:00-16:00	Pod A	24
-2	Monday	08:00-16:00	Pod B	74
-2 Monday	08:00-16:00	Pod C	79
-5 Monday	08:00-16:00	Pod D	92
-1 Monday	16:00-00:00	Pod A	80
-4 Monday	16:00-00:00	Pod B	73
-2	Monday	16:00-00:00	Pod C	36
-5	Monday	15:00-00:00	Pod D	68
-5	Monday	00:00-08:00	Pod A	76
-3	Monday	00:00-08:00	Pod B	76
+
